f3049b33d604ab878a21f63c892eac4c1e1dece7
YC <>
2021-04-27 08:20:02 +0100

    Update settings

--- PineappleJuice/OrganiseData/OrgData.py
+++ PineappleJuice/OrganiseData/OrgData.py
@@ -29,7 +29,6 @@
 # a
 
 
-
 cT1_median = []
 Iron_median = [] 
 PDFF_median = [] 
@@ -40,58 +39,42 @@ Iron_Q3 = []
 PDFF_Q1 = []
 PDFF_Q3 = []
 
-import pdfplumber
-with pdfplumber.open(r'/Users/yi-chunwang/OneDrive - Perspectum Diagnostics Ltd/Python/PineappleJuice/report.pdf') as pdf:
-    first_page = pdf.pages[0]
-    page1_text = first_page.extract_text()
-
-split_text = page1_text.split()
-# '647ms' in split_text # test if it is in the list
-start_index = split_text.index('cT1(ms)') # get the index for better slicing
-
-cT1_median.append(float(split_text[start_index + 4][0:3]))
-Iron_median.append(float(split_text[start_index + 6][0:3]))
-PDFF_median.append(float(split_text[start_index + 8][0:3]))
-cT1_Q1.append(float(split_text[start_index + 9][4:7]))
-cT1_Q3.append(float(split_text[start_index + 9][8:11]))
-Iron_Q1.append(float(split_text[start_index + 10][4:7]))
-Iron_Q3.append(float(split_text[start_index + 10][8:11]))
-PDFF_Q1.append(float(split_text[start_index + 11][4:7]))
-PDFF_Q3.append(float(split_text[start_index + 11][8:11]))
-
 preDataFrame = {}
-preDataFrame['cT1_median (Pre PJ)'] = cT1_median
-preDataFrame['Iron_median (Pre PJ)'] =Iron_median
-preDataFrame['PDFF_median (Pre PJ)'] =PDFF_median
-preDataFrame['cT1_Q1 (Pre PJ)'] = cT1_Q1
-preDataFrame['cT1_Q3 (Pre PJ)'] = cT1_Q3
-preDataFrame['Iron_Q1 (Pre PJ)'] = Iron_Q1
-preDataFrame['Iron_Q3 (Pre PJ)'] = Iron_Q3
-preDataFrame['PDFF_Q1 (Pre PJ)'] = PDFF_Q1
-preDataFrame['PDFF_Q3 (Pre PJ)'] = PDFF_Q3
+
+import os
+import pdfplumber
+
+directory = r'/Users/yi-chunwang/OneDrive - Perspectum Diagnostics Ltd/Python/PineappleJuice/OrganiseData' # read mode
+
+for filename in os.listdir(directory):
+    if filename.endswith('.pdf'):
+        file_to_open = os.path.join(directory,filename)
+        with pdfplumber.open(file_to_open) as report:
+            first_page = report.pages[0]
+            page1_text = first_page.extract_text()
+            split_text = page1_text.split()
+            start_index = split_text.index('cT1(ms)') 
+
+            cT1_median.append(float(split_text[start_index + 4][0:3]))
+            Iron_median.append(float(split_text[start_index + 6][0:3]))
+            PDFF_median.append(float(split_text[start_index + 8][0:3]))
+            cT1_Q1.append(float(split_text[start_index + 9][4:7]))
+            cT1_Q3.append(float(split_text[start_index + 9][8:11]))
+            Iron_Q1.append(float(split_text[start_index + 10][4:7]))
+            Iron_Q3.append(float(split_text[start_index + 10][8:11]))
+            PDFF_Q1.append(float(split_text[start_index + 11][4:7]))
+            PDFF_Q3.append(float(split_text[start_index + 11][8:11]))
+            
+            preDataFrame['cT1_median (Pre PJ)'] = cT1_median
+            preDataFrame['Iron_median (Pre PJ)'] = Iron_median
+            preDataFrame['PDFF_median (Pre PJ)'] = PDFF_median
+            preDataFrame['cT1_Q1 (Pre PJ)'] = cT1_Q1
+            preDataFrame['cT1_Q3 (Pre PJ)'] = cT1_Q3
+            preDataFrame['Iron_Q1 (Pre PJ)'] = Iron_Q1
+            preDataFrame['Iron_Q3 (Pre PJ)'] = Iron_Q3
+            preDataFrame['PDFF_Q1 (Pre PJ)'] = PDFF_Q1
+            preDataFrame['PDFF_Q3 (Pre PJ)'] = PDFF_Q3
+    else:
+        continue
 
 preDataFrame
-
-
-# loop through multiple files in a folder
-
-# fsencode will make the str in byte mode----------
-directory = os.fsencode('/Users/yi-chunwang/OneDrive - Perspectum Diagnostics Ltd/Python/PineappleJuice/OrganiseData') 
-directory
-
-import os
-for filename in os.listdir(directory):
-    if filename.endswith(b'.pdf'): # with the b, Python reads the filename in bytes mode
-        print(os.path.join(directory,filename))
-    else:
-        continue
-
-# use read mode------------------------
-directory2 = r'/Users/yi-chunwang/OneDrive - Perspectum Diagnostics Ltd/Python/PineappleJuice/OrganiseData' # read mode
-import os
-for filename in os.listdir(directory2):
-    if filename.endswith('.pdf'):
-        file_to_open = os.path.join(directory2,filename)
-
-    else:
-        continue

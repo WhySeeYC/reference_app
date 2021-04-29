f8cf89cf50c81048e36238026f3a7db95a674b9b
YC <>
2021-04-29 11:19:12 +0100

    Normalise data extract function

--- PineappleJuice/OrgData.py
+++ PineappleJuice/OrgData.py
@@ -1,61 +1,31 @@
-# loop through json files and write stuff into DataFrame
-# Intersted info: Portal ID, EXP ID, cT1 median, Iron median, PDFF median
-
-
-# file = open('/Users/yi-chunwang/OneDrive - Perspectum Diagnostics Ltd/Python/PineappleJuice/report.tex') 
-# file.split()
-# 
-# count = 0
-# for line in file:
-#     count = count +1
-#     # print(line.split()[-1])
-# print(count)
-# 
-# count = 0
-# for line in file:
-#     if "centering" in line is True:
-#         print('find it!')
-#         count = count+1
-# print(count)
-#         #cT1 = line.split()[5][-3:]
-#         #cT1_Q1 = line.split()[9]
-#         #cT1_Q3 = line.split()[11]
-#     else:
-#         continue
-# print('cT1, cT1_Q1, cT1_Q3')
-# 
-# 
-# a.find('\centering \textbf{\\cT1 (ms)}  \\ Median')
-# a
-
-EXP_number = []
-Portal_ID = []
-cT1_median = []
-Iron_median = [] 
-PDFF_median = [] 
-cT1_Q1 = []
-cT1_Q3 = []
-Iron_Q1 = []
-Iron_Q3 = []
-PDFF_Q1 = []
-PDFF_Q3 = []
-
-preDataFrame = {}
 
 import os
 import pdfplumber
 
-Pre_results_folder = r'/Users/yi-chunwang/OneDrive - Perspectum Diagnostics Ltd/Python/PineappleJuice/Pre_results'
-
-for folder_name in os.listdir(Pre_results_folder):
-    if folder_name.startswith('E'):
-        EXP_number.append(folder_name.split('-')[0])
-        Portal_ID.append(folder_name.split('-')[1])
-        file_to_open = os.path.join(Pre_results_folder, folder_name,'report.pdf')
-        preDataFrame['EXP_number'] = EXP_number
-        preDataFrame['Portal_ID'] = Portal_ID
-
-        try:
+def Extract_Data():
+    EXP_number = []
+    Portal_ID = []
+    cT1_median = []
+    Iron_median = [] 
+    PDFF_median = [] 
+    cT1_Q1 = []
+    cT1_Q3 = []
+    Iron_Q1 = []
+    Iron_Q3 = []
+    PDFF_Q1 = []
+    PDFF_Q3 = []
+    cT1_Ref_Upper = []
+    cT1_Ref_Lower = []
+    Iron_Ref = []
+    PDFF_Ref = []
+    # Results_folder = input("Enter the result folder name:")  # need to have path
+    for folder_name in os.listdir(Results_folder):
+        if folder_name.startswith('E'):
+            EXP_number.append(folder_name.split('-')[0])
+            Portal_ID.append(folder_name.split('-')[1])
+            file_to_open = os.path.join(Results_folder, folder_name,'report.pdf')
+            
+        try:    # Manage the file does not exist situation
             with pdfplumber.open(file_to_open) as report:
                 first_page = report.pages[0]
                 page1_text = first_page.extract_text()
@@ -64,7 +34,6 @@ for folder_name in os.listdir(Pre_results_folder):
                     start_index = split_text.index('cT1(ms)')
                 except:
                     continue
-
                     cT1_median.append(float(split_text[start_index + 4][0:3]))
                     Iron_median.append(float(split_text[start_index + 6][0:3]))
                     PDFF_median.append(float(split_text[start_index + 8][0:3]))
@@ -74,35 +43,67 @@ for folder_name in os.listdir(Pre_results_folder):
                     Iron_Q3.append(float(split_text[start_index + 10][8:11]))
                     PDFF_Q1.append(float(split_text[start_index + 11][4:7]))
                     PDFF_Q3.append(float(split_text[start_index + 11][8:11]))
-       
-                    preDataFrame['cT1_median (Pre PJ)'] = cT1_median
-                    preDataFrame['Iron_median (Pre PJ)'] = Iron_median
-                    preDataFrame['PDFF_median (Pre PJ)'] = PDFF_median
-                    preDataFrame['cT1_Q1 (Pre PJ)'] = cT1_Q1
-                    preDataFrame['cT1_Q3 (Pre PJ)'] = cT1_Q3
-                    preDataFrame['Iron_Q1 (Pre PJ)'] = Iron_Q1
-                    preDataFrame['Iron_Q3 (Pre PJ)'] = Iron_Q3
-                    preDataFrame['PDFF_Q1 (Pre PJ)'] = PDFF_Q1
-                    preDataFrame['PDFF_Q3 (Pre PJ)'] = PDFF_Q3
-       
                     # get the reference values
-                    cT1_Ref_Upper = [float(split_text[58][0:3])]
-                    cT1_Ref_Lower = [float(split_text[58][4:7])]
-                    Iron_Ref = [float(split_text[60][1:4])]
-                    PDFF_Ref = [float(split_text[62][1:4])]
-      
-                    preDataFrame['cT1_Ref_Upper'] = cT1_Ref_Upper
-                    preDataFrame['cT1_Ref_Lower'] = cT1_Ref_Lower
-                    preDataFrame['Iron_Ref'] = Iron_Ref
-                    preDataFrame['PDFF_Ref'] = PDFF_Ref
+                    cT1_Ref_Upper.append(float(split_text[58][0:3]))
+                    cT1_Ref_Lower.append(float(split_text[58][4:7]))
+                    Iron_Ref.append(float(split_text[60][1:4]))
+                    PDFF_Ref.append(float(split_text[62][1:4]))     
         except:
             print('No report in the result folder')
+        else:
+            continue
+    global DataFrame
+    DataFrame = pd.DataFrame()
+    DataFrame['EXP_number'] = EXP_number
+    DataFrame['Portal_ID'] = Portal_ID
+    DataFrame['cT1_median'] = cT1_median
+    DataFrame['Iron_median'] = Iron_median
+    DataFrame['PDFF_median'] = PDFF_median
+    DataFrame['cT1_Q1'] = cT1_Q1
+    DataFrame['cT1_Q3'] = cT1_Q3
+    DataFrame['Iron_Q1'] = Iron_Q1
+    DataFrame['Iron_Q3'] = Iron_Q3
+    DataFrame['PDFF_Q1'] = PDFF_Q1
+    DataFrame['PDFF_Q3'] = PDFF_Q3
+    DataFrame['cT1_Ref_Upper'] = cT1_Ref_Upper
+    DataFrame['cT1_Ref_Lower'] = cT1_Ref_Lower
+    DataFrame['Iron_Ref'] = Iron_Ref
+    DataFrame['PDFF_Ref'] = PDFF_Ref
 
-    else:
-        continue
+
+def preDataFrame():
+    Extract_Data()
+    DataFrame.rename(columns = {
+        'cT1 median':'cT1 median (Pre PJ)'
+        ,'Iron median':'Iron median (Pre PJ)'
+        ,'PDFF median':'PDFF median (Pre PJ)'
+        ,'cT1_Q1':'cT1 Q1 (Pre PJ)'
+        ,'Iron_Q1':'Iron Q1 (Pre PJ)'
+        ,'PDFF_Q1':'PDFF Q1 (Pre PJ)'
+        ,'cT1_Q3':'cT1 Q3 (Pre PJ)'
+        ,'Iron_Q3':'Iron Q3 (Pre PJ)'
+        ,'PDFF_Q3':'PDFF Q3 (Pre PJ)'})
+    global preDataFrame
+    preDataFrame = DataFrame
+
+def postDataFrame():
+    Extract_Data()
+    DataFrame.rename(columns = {
+        'cT1 median':'cT1 median (post PJ)'
+        ,'Iron median':'Iron median (post PJ)'
+        ,'PDFF median':'PDFF median (post PJ)'
+        ,'cT1_Q1':'cT1 Q1 (post PJ)'
+        ,'Iron_Q1':'Iron Q1 (post PJ)'
+        ,'PDFF_Q1':'PDFF Q1 (post PJ)'
+        ,'cT1_Q3':'cT1 Q3 (post PJ)'
+        ,'Iron_Q3':'Iron Q3 (post PJ)'
+        ,'PDFF_Q3':'PDFF Q3 (post PJ)'})
+    global postDataFrame
+    postDataFrame = DataFrame
 
 
-
-preDataFrame
-
+preDataFrame()
 # Write code to manage those files that have only PDFF or only Iron 
+# Expand to Post_result
+
+/Users/yi-chunwang/OneDrive - Perspectum Diagnostics Ltd/Python/PineappleJuice/Pre_results # will also be for post

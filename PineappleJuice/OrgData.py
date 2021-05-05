77a8aece1576b8030c879250cbe82bb7f7869dcc
YC <>
2021-05-05 13:09:43 +0100

    Merge Pre and Post PJ with outer merge method

--- PineappleJuice/OrgData.py
+++ PineappleJuice/OrgData.py
@@ -1,6 +1,11 @@
 
 import os
 import pdfplumber
+import pandas as pd
+
+os.getcwd() # check current working directory
+os.chdir('/Users/yi-chunwang/OneDrive - Perspectum Diagnostics Ltd/Python/PineappleJuice')  # change current working directory in Python
+
 
 def Extract_Data():
     EXP_number = []
@@ -18,13 +23,12 @@ def Extract_Data():
     cT1_Ref_Lower = []
     Iron_Ref = []
     PDFF_Ref = []
-    # Results_folder = input("Enter the result folder name:")  # need to have path
+    cwd = os.getcwd()
+    userfolder = input("Enter the result folder name:")
+    Results_folder = cwd + '/' + userfolder
     for folder_name in os.listdir(Results_folder):
         if folder_name.startswith('E'):
-            EXP_number.append(folder_name.split('-')[0])
-            Portal_ID.append(folder_name.split('-')[1])
             file_to_open = os.path.join(Results_folder, folder_name,'report.pdf')
-            
         try:    # Manage the file does not exist situation
             with pdfplumber.open(file_to_open) as report:
                 first_page = report.pages[0]
@@ -32,8 +36,8 @@ def Extract_Data():
                 split_text = page1_text.split()
                 try:  # Manage the exception that the automatic segmented report is not complete
                     start_index = split_text.index('cT1(ms)')
-                except:
-                    continue
+                    EXP_number.append(folder_name.split('-')[0])
+                    Portal_ID.append(folder_name.split('-')[1])
                     cT1_median.append(float(split_text[start_index + 4][0:3]))
                     Iron_median.append(float(split_text[start_index + 6][0:3]))
                     PDFF_median.append(float(split_text[start_index + 8][0:3]))
@@ -47,63 +51,55 @@ def Extract_Data():
                     cT1_Ref_Upper.append(float(split_text[58][0:3]))
                     cT1_Ref_Lower.append(float(split_text[58][4:7]))
                     Iron_Ref.append(float(split_text[60][1:4]))
-                    PDFF_Ref.append(float(split_text[62][1:4]))     
+                    PDFF_Ref.append(float(split_text[62][1:4])) 
+                except:
+                    continue   
         except:
             print('No report in the result folder')
         else:
             continue
-    global DataFrame
-    DataFrame = pd.DataFrame()
-    DataFrame['EXP_number'] = EXP_number
-    DataFrame['Portal_ID'] = Portal_ID
-    DataFrame['cT1_median'] = cT1_median
-    DataFrame['Iron_median'] = Iron_median
-    DataFrame['PDFF_median'] = PDFF_median
-    DataFrame['cT1_Q1'] = cT1_Q1
-    DataFrame['cT1_Q3'] = cT1_Q3
-    DataFrame['Iron_Q1'] = Iron_Q1
-    DataFrame['Iron_Q3'] = Iron_Q3
-    DataFrame['PDFF_Q1'] = PDFF_Q1
-    DataFrame['PDFF_Q3'] = PDFF_Q3
-    DataFrame['cT1_Ref_Upper'] = cT1_Ref_Upper
-    DataFrame['cT1_Ref_Lower'] = cT1_Ref_Lower
-    DataFrame['Iron_Ref'] = Iron_Ref
-    DataFrame['PDFF_Ref'] = PDFF_Ref
-
+    global DataDict
+    DataDict = {}
+    DataDict['EXP_number'] = EXP_number
+    DataDict['Portal_ID'] = Portal_ID
+    DataDict['cT1_median'] = cT1_median
+    DataDict['Iron_median'] = Iron_median
+    DataDict['PDFF_median'] = PDFF_median
+    DataDict['cT1_Q1'] = cT1_Q1
+    DataDict['cT1_Q3'] = cT1_Q3
+    DataDict['Iron_Q1'] = Iron_Q1
+    DataDict['Iron_Q3'] = Iron_Q3
+    DataDict['PDFF_Q1'] = PDFF_Q1
+    DataDict['PDFF_Q3'] = PDFF_Q3
+    DataDict['cT1_Ref_Upper'] = cT1_Ref_Upper
+    DataDict['cT1_Ref_Lower'] = cT1_Ref_Lower
+    DataDict['Iron_Ref'] = Iron_Ref
+    DataDict['PDFF_Ref'] = PDFF_Ref
 
 def preDataFrame():
     Extract_Data()
-    DataFrame.rename(columns = {
-        'cT1 median':'cT1 median (Pre PJ)'
-        ,'Iron median':'Iron median (Pre PJ)'
-        ,'PDFF median':'PDFF median (Pre PJ)'
-        ,'cT1_Q1':'cT1 Q1 (Pre PJ)'
-        ,'Iron_Q1':'Iron Q1 (Pre PJ)'
-        ,'PDFF_Q1':'PDFF Q1 (Pre PJ)'
-        ,'cT1_Q3':'cT1 Q3 (Pre PJ)'
-        ,'Iron_Q3':'Iron Q3 (Pre PJ)'
-        ,'PDFF_Q3':'PDFF Q3 (Pre PJ)'})
-    global preDataFrame
-    preDataFrame = DataFrame
+    preData = pd.DataFrame(DataDict) # to put dictionary in a dataframe, all arrays mist be the same length
+    b = preData.rename(columns = {'EXP_number':'EXP_number (Pre PJ)', 'Portal_ID': 'Portal_ID (Pre PJ)', 'cT1_median':'cT1 median (Pre PJ)','Iron_median':'Iron median (Pre PJ)','PDFF_median':'PDFF median (Pre PJ)','cT1_Q1':'cT1 Q1 (Pre PJ)','Iron_Q1':'Iron Q1 (Pre PJ)','PDFF_Q1':'PDFF Q1 (Pre PJ)','cT1_Q3':'cT1 Q3 (Pre PJ)','Iron_Q3':'Iron Q3 (Pre PJ)','PDFF_Q3':'PDFF Q3 (Pre PJ)'})
+    global prePJ
+    prePJ = b
+
 
 def postDataFrame():
     Extract_Data()
-    DataFrame.rename(columns = {
-        'cT1 median':'cT1 median (post PJ)'
-        ,'Iron median':'Iron median (post PJ)'
-        ,'PDFF median':'PDFF median (post PJ)'
-        ,'cT1_Q1':'cT1 Q1 (post PJ)'
-        ,'Iron_Q1':'Iron Q1 (post PJ)'
-        ,'PDFF_Q1':'PDFF Q1 (post PJ)'
-        ,'cT1_Q3':'cT1 Q3 (post PJ)'
-        ,'Iron_Q3':'Iron Q3 (post PJ)'
-        ,'PDFF_Q3':'PDFF Q3 (post PJ)'})
-    global postDataFrame
-    postDataFrame = DataFrame
-
+    postData = pd.DataFrame(DataDict).rename(columns = {'EXP_number':'EXP_number (Post PJ)', 'Portal_ID': 'Portal_ID (Post PJ)', 'cT1_median':'cT1 median (Post PJ)','Iron_median':'Iron median (Post PJ)','PDFF_median':'PDFF median (Post PJ)','cT1_Q1':'cT1 Q1 (Post PJ)','Iron_Q1':'Iron Q1 (Post PJ)','PDFF_Q1':'PDFF Q1 (Post PJ)','cT1_Q3':'cT1 Q3 (Post PJ)','Iron_Q3':'Iron Q3 (Post PJ)','PDFF_Q3':'PDFF Q3 (Post PJ)'})
+    global postPJ
+    postPJ = postData
 
 preDataFrame()
-# Write code to manage those files that have only PDFF or only Iron 
-# Expand to Post_result
+postDataFrame()
 
-/Users/yi-chunwang/OneDrive - Perspectum Diagnostics Ltd/Python/PineappleJuice/Pre_results # will also be for post
+# get record ID dataframe
+OriginFile = pd.read_csv('/Users/yi-chunwang/OneDrive - Perspectum Diagnostics Ltd/Python/PineappleJuice/Pineapple juice effect.csv')
+PreRecord_ID = OriginFile.iloc[0:, :2].rename(columns = {'Portal ID LMS0 (pre PJ)':'Portal_ID (Pre PJ)'})
+PostRecord_ID = OriginFile.iloc[0:, [0,3]].rename(columns = {'Portal ID LMS1 (post PJ)':'Portal_ID (Post PJ)'})
+# Merge DataFrame 
+PreRecord = PreRecord_ID.merge(prePJ, on = ['Portal_ID (Pre PJ)'], how = 'outer')
+PostRecord = PostRecord_ID.merge(postPJ, on = ['Portal_ID (Post PJ)'], how = 'outer')
+
+Outer2 = PreRecord.merge(PostRecord, on = ['Record Id'], how = 'outer', copy = False)
+# Outer = PreRecord.merge(PostRecord, on = ['Record Id', 'cT1_Ref_Upper', 'cT1_Ref_Lower', 'Iron_Ref', 'PDFF_Ref'], how = 'outer') -> does not work
